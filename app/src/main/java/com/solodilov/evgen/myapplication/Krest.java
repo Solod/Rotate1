ee92eff050d40a4d79f870d948c435dbbcd971a3
evgen.solodilov <evgen.solodilov@gmail.com>
2017-02-09 16:51:56 +0200

    Gwin the Best

--- app/src/main/java/com/solodilov/evgen/myapplication/Krest.java
+++ app/src/main/java/com/solodilov/evgen/myapplication/Krest.java
@@ -5,14 +5,15 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PointF;
 import android.graphics.RectF;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 
 public class Krest extends View implements View.OnTouchListener {
-
+    ACTION action;
+    String str = "TA-DAAAA";
     Paint p;
     Path path;
     RectF rectF;
@@ -26,6 +27,7 @@ public class Krest extends View implements View.OnTouchListener {
         p = new Paint();
         p.setStrokeWidth(3);
         p.setStyle(Paint.Style.STROKE);
+        p.setTextSize(60);
         rectF = new RectF(300, 300, 600, 800);
         path = new Path();
         matrix = new Matrix();
@@ -38,19 +40,21 @@ public class Krest extends View implements View.OnTouchListener {
         canvas.drawARGB(80, 102, 204, 255);
         path.reset();
         path.addRect(rectF, Path.Direction.CW);
+        path.addCircle(rectF.right, rectF.bottom, 50, Path.Direction.CW);
         matrix.reset();
         matrix.setRotate(rotate, rectF.centerX(), rectF.centerY());
         path.transform(matrix);
         canvas.restore();
+        canvas.drawTextOnPath(str, path, 0, rectF.height() / 2, p);
         canvas.drawPath(path, p);
     }
-
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+if()
                 startDegree = getDegree(rectF, event);
                 break;
             case MotionEvent.ACTION_MOVE:
@@ -80,6 +84,17 @@ public class Krest extends View implements View.OnTouchListener {
         double radians = Math.atan2(delta_y, delta_x);//
         double degrees = Math.toDegrees(radians);
         return (float) degrees;
+    }
+
+    private boolean isCircle(PointF point) {
+        float dx = point.x - rectF.right;
+        float dy = point.y - rectF.left;
+
+        return Math.sqrt(dx * dx + dy * dy) <= 50;
+    }
+
+    enum ACTION {
+        RECT, CIRCLE
     }
 }
 
